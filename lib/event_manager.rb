0b750f1b8c8f886986b0d6e5905f3070f9192ac3
Katie Lonsdale <katie952@gmail.com>
2026-04-14 13:42:32 -0600

    refactor format_zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,14 +2,7 @@ require 'csv'
 puts "EventManager Initialized!"
 
 def format_zipcode(zipcode)
-  if zipcode.nil?
-    zipcode = "00000"
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust(5, "0")
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  end
-  zipcode
+  zipcode.to_s.rjust(5,"0")[0..4]
 end
 
 contents = CSV.open"event_attendees.csv", headers: true, header_converters: :symbol
